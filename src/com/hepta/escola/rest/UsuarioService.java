1f9756b54cdd287a355ade6b4ebe5b3206475071
Matheus Alves Dos Santos <matheus.santos@hepta.com.br>
2020-02-07 11:08:09 -0300

    finalizando o backend de usuarios (test e rest)

--- src/com/hepta/escola/rest/UsuarioService.java
+++ src/com/hepta/escola/rest/UsuarioService.java
@@ -1,5 +1,137 @@
 package com.hepta.escola.rest;
 
-public class UsuarioService {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import com.hepta.escola.entity.Usuario;
+import com.hepta.escola.persistence.UsuarioDAO;
+
+
+
+@Path("/usuarios")
+public class UsuarioService {
+	
+    @Context
+    private HttpServletRequest request;
+
+    @Context
+    private HttpServletResponse response;
+    
+    protected void setRequest(HttpServletRequest request) {
+        this.request = request;
+    }
+    
+    
+    private Usuario usuario = new Usuario();
+    private UsuarioDAO userDao;
+    
+    
+    public UsuarioService() {
+    	userDao = new UsuarioDAO(Usuario.class);
+    }
+    
+    
+    
+    /**
+     * Cria um usuario no sistema
+     * @param usuario
+     * @return 200 conseguiu adicionar
+     */
+    @Path("/")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @POST
+    public Response usuarioCreate(Usuario usuario) {
+    	try {
+    		userDao.save(usuario);
+    	}catch(Exception e){
+    		e.printStackTrace();
+    		System.out.println("Erro");
+    		return Response.status(Status.INTERNAL_SERVER_ERROR)
+    				.entity("Erro ao criar usuario").build();
+    		
+    	}
+    	return Response.status(Status.OK).build();
+    }
+    /**
+     * Lista todos os usuarios do sistema
+     * @return
+     */
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+    @GET
+    public Response usuarioRead() {
+    	List<Usuario> usuarios = new ArrayList<>();
+    	try {
+    		usuarios = userDao.getAll();
+    	}catch(Exception e) {
+    		return Response.status(Status.INTERNAL_SERVER_ERROR)
+    				.entity("Erro ao buscar usuarios").build();
+    	}
+    	GenericEntity<List<Usuario>> entity = new GenericEntity<List<Usuario>>(usuarios) {};
+    	return Response.status(Status.OK).entity(entity).build();
+    }
+    /**
+     * 
+     * @param id
+     * @return
+     */
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)	
+    @DELETE
+    public Response usuarioDelete(@PathParam("id") Integer id) {
+    	try {
+    		userDao.delete(id);
+    	}catch(Exception e) {
+    		return Response.status(Status.INTERNAL_SERVER_ERROR)
+    				.entity("Erro ao deletar usuario").build();
+    	}
+    	return Response.status(Status.OK).build();
+    }
+    /**
+     * Altera um usuario
+     * @param id
+     * @return
+     */
+    @Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@PUT
+    public Response usuarioUpdate(@PathParam("id") Integer id) {
+    	try {
+    		setUsuario(userDao.update(usuario));
+    	}catch(Exception e){
+    		e.printStackTrace();
+    		return Response.status(Status.INTERNAL_SERVER_ERROR)
+    				.entity("Erro ao alterar usuario")
+    				.build();
+    	}
+    	return Response.status(Status.OK).build();
+    }
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+    
+    
 }
--- src/com/hepta/escola/test/UsuarioServiceTest.java
+++ src/com/hepta/escola/test/UsuarioServiceTest.java
@@ -1,5 +1,51 @@
 package com.hepta.escola.test;
 
-public class UsuarioServiceTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+
+import org.glassfish.jersey.client.ClientConfig;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.hepta.escola.entity.Usuario;
+import com.hepta.escola.enums.TipoUsuario;
+
+public class UsuarioServiceTest {
+	private static WebTarget service;
+	private static final String URL_LOCAL = "http://localhost:8080/escola/rs/usuarios";
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception{
+		ClientConfig config = new ClientConfig();
+		Client client = ClientBuilder.newClient(config);
+		service = client.target(UriBuilder.fromUri(URL_LOCAL).build());
+	}
+	
+	public Usuario usuarioCreate() {
+		Usuario usuario = new Usuario();
+		usuario.setUsername("admin");
+		usuario.setSenha("admin");
+		usuario.setTipo(TipoUsuario.ADMINISTRADOR);
+		
+		
+		return usuario;
+	}
+	@Test	
+	void testCadastroUsuario() {
+		Usuario usuario = usuarioCreate();
+		Response response = service.request().post(Entity.entity(usuario,
+				MediaType.APPLICATION_JSON_TYPE));
+		assertEquals(Response.Status.OK.getStatusCode(), response.getStatusInfo().getStatusCode());
+		
+	}
+	
+	
+	
 }
