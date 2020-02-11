3597eb7ff95ec26d05f6f037df2d790fd6be981a
Matheus Alves Dos Santos <matheus.santos@hepta.com.br>
2020-02-11 16:33:44 -0300

    Adicionando template da tela de Administrador e professor

--- WebContent/resources/js/index.js
+++ WebContent/resources/js/index.js
@@ -5,6 +5,10 @@ var main = new Vue({
                 password:" ",
                 tipo:" ",
                 isAuth:false},
+        professores:[],
+        alunos:[],
+        urlProfessores: "http://localhost:8080/escola/rs/professores",
+        urlAlunos:"http://localhost:8080/escola/rs/alunos"
     //usuario = require('../resources/js/login.js').usuario
     },
     created: function(){
@@ -17,11 +21,43 @@ var main = new Vue({
         }else{
             vm.usuarioLogado = userAux;
         }
+        vm.getProfessores();
+        vm.getAlunos();
     },
     methods:{
         sair: function(){
             localStorage.clear();
             document.location.href="/escola/login.html"
+        },
+        getProfessores: function(){
+            var xhr = new XMLHttpRequest();
+            var vm = this;
+            xhr.open('GET',vm.urlProfessores,true);
+            xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+            xhr.onload = function(){
+                vm.professores = JSON.parse(xhr.responseText);
+                if (xhr.readyState == 4 && xhr.status == "200") {
+                    console.table(vm.professores);
+                } else {
+                    console.error(vm.professores);
+                }
+            }
+            xhr.send();
+        },
+        getAlunos: function(){
+            var xhr = new XMLHttpRequest();
+            var vm = this;
+            xhr.open('GET',vm.urlAlunos,true);
+            xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+            xhr.onload = function(){
+                vm.alunos = JSON.parse(xhr.responseText);
+                if (xhr.readyState == 4 && xhr.status == "200") {
+                    console.table(vm.alunos);
+                } else {
+                    console.error(vm.alunos);
+                }
+            }
+            xhr.send();
         }
     }
 })
